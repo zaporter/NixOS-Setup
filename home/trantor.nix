b0836d5883a01ade528cf904af81b7c43e67a6b1
zaporter <opensource@zackporter.com>
2023-02-12 21:17:21 -0500

    readme adjustments

--- home/trantor.nix
+++ home/trantor.nix
@@ -4,4 +4,22 @@
         ./features/desktop
         ./features/extended
     ];
+    #monitors = [
+    #    {
+    #      name = "DP-1";
+    #      width = 1920;
+    #      height = 1080;
+    #      x = 0;
+    #      workspace = "2";
+    #    }
+    #    {
+    #      name = "DP-2";
+    #      width = 3840;
+    #      height = 2160;
+    #      x = 1920;
+    #      isPrimary = true;
+    #      scale = "2.0";
+    #      workspace = " 1";
+    #    }
+    #];
 }
